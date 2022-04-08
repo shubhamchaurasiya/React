4ea94df9d616878f23efad58fc18e8cd92506dab
shubhamchaurasiya <cshubham832@gmail.com>
2022-04-08 17:33:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Navigation from './components/Navigation';
+import SingleProduct from './components/SingleProduct';
+import Cart from './pages/Cart';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Router>
+        <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact></Route>
+      
+            <Route path="/products" exact component={Products} ></Route>
+            <Route path="/products/:id"  component={SingleProduct} ></Route>
+            <Route path="/cart" component={Cart} ></Route>
+          </Switch>
+        </Router>
+      </>
   );
 }
 
---
+++ src/components/SingleProduct.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { useState, useEffect } from "react";
+
+const SingleProduct = () => {
+    const [product, setProduct] = useState({});
+    const params =  useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${params.id}`)
+        .then(res => res.json())
+        .then(product => {
+            setProduct(product);
+        })
+    }, [params.id]);
+    
+    return (
+            <>
+                <div className="container mx-auto mt-12">
+                    <button className="mb-12 font-bold" onClick={ () => { history.goBack() } } >Back</button>
+                    <div className="flex">
+                        <img src={ product.image } alt="pizza" />
+                        <div className="ml-16">
+                            <h1 className="text-xl font-bold">{ product.title }</h1>
+                            <div className="text-md">{ product.category }</div>
+                            <div className="text-md">{ product.description }</div>
+                            <div className="font-bold mt-2">₹ { product.price }</div>
+                            <button className="bg-yellow-500 py-1 px-8 rounded-full font-bold mt-4">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+export default SingleProduct
